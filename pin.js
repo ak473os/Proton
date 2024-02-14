93af64f37de32a45662432bc3154d60877e275d7
Bearcat <bwigal2@nastudents.org>
2024-02-14 10:40:41 -0500

    Update pin.js

--- pin.js
+++ pin.js
@@ -3,9 +3,11 @@
 let pass = prompt("Restricted Access! Enter the password to enter this site!");
 let pwd = "ProtonStaff1";
 
-if (pass.toLowerCase() == pwd.toLowerCase()) {
+if (pass === null) { // Check for canceled prompt
+  location.reload(); 
+} else if (pass.toLowerCase() == pwd.toLowerCase()) { 
   alert('Welcome Staff!');
 } else {
-  alert('Incorrect password. Access denied.'); 
-  location.reload();
-}
+  alert('Incorrect password. Access denied.'); 
+  location.reload(); 
+} 
